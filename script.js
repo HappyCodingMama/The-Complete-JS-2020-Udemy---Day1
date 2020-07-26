eb7722c084ae7a037f14cde9a57fcbafe54b1789
HappyCodingMama <happycodingmama@gmail.com>
2020-07-27 01:49:52 +0900

    solving quizzes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 Coding Challenge 1
 
 Mark and John are trying to compare there BMI(Body Mass Index),
-which is calculated using the formula: 
+which is calculated using the formula:
 BMI = mass / height^2 = mass / (height * height).
 (mass in kg and height in meter)
 
@@ -39,7 +39,7 @@ has a higher BMI than John.
 
 
 
-/* 
+/*
 Coding Challenge 2
 John and Mike both play basketball in different teams. In the latest 3 games,
 John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94
@@ -49,10 +49,10 @@ and 123 points.
 2.Decide which team wins in average(highest average score), and
 print the winner to the console.
 Also include the average score in the output.
-3. Then change the scores to show different winners. 
+3. Then change the scores to show different winners.
 Don't forget to take into account there might be a draw(the same average score)
 
-4.EXTRA: Mary also plays basketball, and her team scored 97, 134 and 
+4.EXTRA: Mary also plays basketball, and her team scored 97, 134 and
 105 points. Like before, log the average winner to the console.
 HINT: you will need the && operator to take the decision.
 5. Like before, change the scores to generate different winners, keeping in mind
@@ -105,6 +105,89 @@ there might be draws.
 //     } else {
 //     console.log("All teams got the same average." ); 
 //     }
+
+/*
+    1.declaration function 'calculateAge'
+    using return
+    2. Calulate age of John, Mike and Jane
+    3.declaration function 'yearsUntilRetirement'
+    4.print a string to the console
+    if someone is already retired, then use other string.
+*/
+
+/******* Do not Repeat Yourself *********/
+
+// function calculateAge(birthYear) {
+//   return 2020 - birthYear;
+// }
+
+// var ageJohn = calculateAge(1950);
+// var ageMike = calculateAge(1955);
+// var ageJane = calculateAge(1974);
+
+// console.log(ageJohn, ageMike, ageJane);
+
+
+
+
+// function yearsUntilRetirement(birthYear, firstName) {
+//   var age = calculateAge(birthYear);
+//   var retirement = 65 - age;
+
+//   if (retirement < 0) {
+//     console.log(firstName + ' is already retired ' + -retirement + ' years ago.');
+//   } else if (retirement > 0) {
+//     console.log(firstName + ' retires in ' + retirement + ' years.');
+//   } else {
+//     console.log(firstName + ' is retired this year.');
+//   }
+// }
+
+// yearsUntilRetirement(1950, 'John');
+// yearsUntilRetirement(1955, 'Mike');
+// yearsUntilRetirement(1974, 'Jane');
+
+
+/* coding challenge 3
+John and his family went on a holiday and went to 3 different restaurants. the bills were $124, $48 and $268.
+
+To tip the waiter a fair amount, John created a simple tip calculator
+(as a function). He likes to tip 20% of the bill when the bill is less than $50. 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+In the end, John would like to have 2 arrays
+1) Containing all three tips(one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+(Note: To calculate 20% of a value,
+      simply multiply if with 20/100 = 0.2) */
+
+// var bills = [124, 48, 268];
+
+// function tipCalculator(bill) {
+//   var percentage;
+//   if (bill < 50) {
+//     percentage = 0.2;
+//   } else if (bill >= 50 && bill < 200) {
+//     percentage = 0.15;
+//   } else {
+//     percentage = 0.1;
+//   }
+//   return bill * percentage;
+// }
+
+// var tips = [
+//   tipCalculator(bills[0]),
+//   tipCalculator(bills[1]),
+//   tipCalculator(bills[2])
+// ];
+
+// var totalBills = [
+//   bills[0] + tips[0],
+//   bills[1] + tips[1],
+//   bills[2] + tips[2]
+// ];
+
+// console.log(tips, totalBills);
+
 
 
 
